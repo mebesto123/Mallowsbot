39b595423028c676ee2714ba0687459ca58b4fc3
Jake Sebald <mebesto123@gmail.com>
2022-09-05 04:34:42 -0400

    remove admin added

--- AdminTools.py
+++ AdminTools.py
@@ -9,13 +9,16 @@ async def AdminTools(message, repoPath):
 
     if cmd == "addrole":
         await addAdminRole(message, repoPath)
-
+    elif cmd == "removerole":
+        await removeAdminRole(message, repoPath)
     # if message.content.lower().startswith("-help admin") or cmd is None:
     #     await AdminHelp(message)
 
 async def AdminRectionConfirms(reaction, user, repoPath):
     if reaction.message.embeds[0].title == 'Role Confirmation' and reaction.message.content == '':
             await confirmAdminRole(reaction, repoPath)
+    elif reaction.message.embeds[0].title == 'Role Remove' and reaction.message.content == '':
+            await confirmRemoveAdminRole(reaction, repoPath)
 
 def adminRolesByGuild(guildId, repoPath):
     df = pd.read_csv(repoPath + os.path.sep + "GuildAdminRoles.csv")
@@ -55,17 +58,58 @@ async def addAdminRole(message, repoPath):
     for emoji in emojis:
         await m.add_reaction(emoji)
 
+async def removeAdminRole(message, repoPath):
+    serverRoles = [ x.name.lower() for x in message.guild.roles]
+    adminRoles = [x.lower() for x in adminRolesByGuild(message.guild.id, repoPath)]
+    if message.content.split(" ", 2 )[2] == None:
+        await EasyErrors.easyError(message, "You need to provide a that exists role.")
+        return
+        
+    if str(message.content.split(" ", 2 )[2]).lower() not in serverRoles:
+        await EasyErrors.easyError(message, "You need to provide a that exists role.")
+        return
+    
+    if str(message.content.split(" ", 2 )[2]).lower() not in adminRoles:
+        await EasyErrors.easyError(message, "This role is already not an admin")
+        return
+
+    embed = discord.Embed(
+        title="Role Remove",
+        color=discord.Color.blurple())
+    embed.add_field(name="Remove role: " + str(message.content.split(" ", 2 )[2]),value="Select the green check box to confirm or red to cancel the request.")
+    m = await message.channel.send(embed=embed)
+    emojis = ['✅','🚫']
+
+    for emoji in emojis:
+        await m.add_reaction(emoji)
+
 async def confirmAdminRole(reaction, repoPath):
 
     if reaction.emoji == '✅':
         try:
             roleName = reaction.message.embeds[0].fields[0].name.replace("Comfirm role: ", "")
-            role = next((x for x in reaction.message.guild.roles if x.name.lower() == roleName), None)
+            role = next((x for x in reaction.message.guild.roles if x.name.lower() == roleName.lower()), None)
             df = pd.read_csv(repoPath + os.path.sep + "GuildAdminRoles.csv")
             row = {"Guild": reaction.message.guild.id,"Role": role.name}
             df = df.append(row, ignore_index=True)
             df.to_csv(repoPath + os.path.sep + "GuildAdminRoles.csv", index=False)
             await reaction.message.edit(content="Role " + role.name +  " Added as Admin ✅")
+        except:
+            await EasyErrors.easyError(reaction.message, "An unhandled Error ooccurred")
+    elif reaction.emoji == '🚫':
+        await reaction.message.edit(content="Cancelled 🚫")
+
+async def confirmRemoveAdminRole(reaction, repoPath):
+
+    if reaction.emoji == '✅':
+        try:
+            roleName = reaction.message.embeds[0].fields[0].name.replace("Remove role: ", "")
+            role = next((x for x in reaction.message.guild.roles if x.name.lower() == roleName.lower()), None)
+            df = pd.read_csv(repoPath + os.path.sep + "GuildAdminRoles.csv")
+            row = df[(df.Guild == reaction.message.guild.id) & (df.Role == role.name)].index
+            df = df.drop(labels=row[0], axis=0)
+            df.to_csv(repoPath + os.path.sep + "GuildAdminRoles.csv", index=False)
+            await reaction.message.edit(content="Role " + role.name +  " has been removed as Admin ✅")
         except:
             await EasyErrors.easyError(reaction.message, "An unhandled Error ooccurred")
     elif reaction.emoji == '🚫':
